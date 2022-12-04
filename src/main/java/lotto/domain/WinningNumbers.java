780089861838d2d720f2e626916600a726d9b215
2jun0 <soo28819@gmail.com>
2022-12-04 22:26:26 +0900

    Feat(당첨 번호의 중복 여부 검증):  당첨 번호의 중복 여부 검증 구현.

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -13,6 +13,7 @@ public class WinningNumbers {
     public WinningNumbers(List<Integer> winningNumbers) {
         validateWinningNumbersSize(winningNumbers);
         validateWinningNumbersRange(winningNumbers);
+        validateWinningNumbersDuplicate(winningNumbers);
 
         this.winningNumbers = winningNumbers;
     }
@@ -33,6 +34,12 @@ public class WinningNumbers {
                 || winningNumber > LOTTO_MAX_NUMBER_INCLUSIVE) {
             throw new IllegalArgumentException(String.format("당첨번호는 %d부터 %d사이의 정수여야 합니다.",
                     LOTTO_MIN_NUMBER_INCLUSIVE, LOTTO_MAX_NUMBER_INCLUSIVE));
+        }
+    }
+
+    private void validateWinningNumbersDuplicate(List<Integer> winningNumbers) {
+        if (winningNumbers.stream().distinct().count() != winningNumbers.size()) {
+            throw new IllegalArgumentException("당첨번호는 중복된 숫자가 들어가면 안됩니다.");
         }
     }
 
