f5f934bd3187576edfdff6bf8aa73727b1176438
2jun0 <soo28819@gmail.com>
2022-12-04 22:54:10 +0900

    Test(당첨 번호): 당첨번호 클래스의 단위 테스트 작성.

---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,80 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class WinningNumbersTest {
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    class CreateTest {
+
+        @ParameterizedTest(name = "failWithInvalidSize {argumentsWithNames}")
+        @MethodSource("provideFailWithInvalidSize")
+        void failWithInvalidSize(List<Integer> list) {
+            Assertions.assertThatThrownBy(() -> new WinningNumbers(list))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("당첨번호의 개수는 6여야 합니다.");
+        }
+
+        Stream<Arguments> provideFailWithInvalidSize() {
+            return Stream.of(
+                    Arguments.of(List.of(1, 2, 3, 4, 5)),
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 6, 7)));
+        }
+
+        @ParameterizedTest(name = "failWithDuplicateNumbers {argumentsWithNames}")
+        @MethodSource("provideFailWithDuplicateNumbers")
+        void failWithDuplicateNumbers(List<Integer> list) {
+            Assertions.assertThatThrownBy(() -> new WinningNumbers(list))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("당첨번호는 중복된 숫자가 들어가면 안됩니다.");
+        }
+
+        Stream<Arguments> provideFailWithDuplicateNumbers() {
+            return Stream.of(
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 5)),
+                    Arguments.of(List.of(1, 2, 3, 1, 5, 6)));
+        }
+
+        @ParameterizedTest(name = "failWithInvalidRange {argumentsWithNames}")
+        @MethodSource("provideFailWithInvalidRange")
+        void failWithInvalidRange(List<Integer> list) {
+            Assertions.assertThatThrownBy(() -> new WinningNumbers(list))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("당첨번호는 1부터 45사이의 정수여야 합니다.");
+        }
+
+        Stream<Arguments> provideFailWithInvalidRange() {
+            return Stream.of(
+                    Arguments.of(List.of(0, 2, 3, 4, 5, 6)),
+                    Arguments.of(List.of(1, 2, 3, 4, 5, 46)));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideMatchCount")
+    void matchCount(List<Integer> list, Lotto lotto, int expect) {
+        WinningNumbers winningNumbers = new WinningNumbers(list);
+        int matchCount = winningNumbers.matchCount(lotto);
+
+        Assertions.assertThat(matchCount).isEqualTo(expect);
+    }
+
+    static Stream<Arguments> provideMatchCount() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 45, 44, 43)), 3),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(45, 44, 43, 42, 41, 40)), 0),
+                Arguments.of(List.of(1, 2, 3, 4, 5, 6),
+                        new Lotto(List.of(1, 2, 3, 4, 5, 6)), 6));
+    }
+}
