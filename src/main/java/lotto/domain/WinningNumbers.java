22f2a8831cacf5451b629b9893cc6f14a8f334c1
2jun0 <soo28819@gmail.com>
2022-12-04 22:22:55 +0900

    Feat(당첨 번호의 번호 범위 검증): 당첨 번호의 번호 범위 검증 구현.

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.domain.Lotto.LOTTO_MAX_NUMBER_INCLUSIVE;
+import static lotto.domain.Lotto.LOTTO_MIN_NUMBER_INCLUSIVE;
+import static lotto.domain.Lotto.LOTTO_NUMBER_COUNT;
+
 import java.util.List;
 
 public class WinningNumbers {
@@ -8,14 +12,27 @@ public class WinningNumbers {
 
     public WinningNumbers(List<Integer> winningNumbers) {
         validateWinningNumbersSize(winningNumbers);
+        validateWinningNumbersRange(winningNumbers);
 
         this.winningNumbers = winningNumbers;
     }
 
     private void validateWinningNumbersSize(List<Integer> winningNumbers) {
-        if (winningNumbers.size() != Lotto.LOTTO_NUMBER_COUNT) {
+        if (winningNumbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(
-                    String.format("당첨번호의 개수는 %d여야 합니다.", Lotto.LOTTO_NUMBER_COUNT));
+                    String.format("당첨번호의 개수는 %d여야 합니다.", LOTTO_NUMBER_COUNT));
+        }
+    }
+
+    private void validateWinningNumbersRange(List<Integer> winningNumbers) {
+        winningNumbers.forEach(this::validateWinningNumberRange);
+    }
+
+    private void validateWinningNumberRange(int winningNumber) {
+        if (LOTTO_MIN_NUMBER_INCLUSIVE > winningNumber
+                || winningNumber > LOTTO_MAX_NUMBER_INCLUSIVE) {
+            throw new IllegalArgumentException(String.format("당첨번호는 %d부터 %d사이의 정수여야 합니다.",
+                    LOTTO_MIN_NUMBER_INCLUSIVE, LOTTO_MAX_NUMBER_INCLUSIVE));
         }
     }
 
