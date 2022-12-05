702ecfcdb4746dcc85f4c80809920612d73e7098
2jun0 <soo28819@gmail.com>
2022-12-05 21:45:10 +0900

    Feat(보너스 번호): 보너스 번호 검증 추가.

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -5,7 +5,19 @@ public class BonusNumber {
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
+        validateBonusNumber(bonusNumber);
+
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (Lotto.LOTTO_MIN_NUMBER_INCLUSIVE > bonusNumber
+                || bonusNumber > Lotto.LOTTO_MAX_NUMBER_INCLUSIVE) {
+            throw new IllegalArgumentException(
+                    String.format("보너스 번호는 %d부터 %d까지여야 합니다.",
+                            Lotto.LOTTO_MIN_NUMBER_INCLUSIVE,
+                            Lotto.LOTTO_MAX_NUMBER_INCLUSIVE));
+        }
     }
 
     public boolean match(Lotto lotto) {
